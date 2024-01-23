edfdf552a23fce1c34d02867d3ae63547ced6ba0
inflzcoder <sandersz.old@rambler.ru>
2024-01-23 19:59:54 +0500

    Added Rotten Flesh variants for Husk and Drowned

---
+++ src/main/java/net/sawilove/meatmod/item/DriedfleshItem.java
@@ -0,0 +1,31 @@
+
+package net.sawilove.meatmod.item;
+
+import net.sawilove.meatmod.procedures.DriedfleshPlayerFinishesUsingItemProcedure;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.LivingEntity;
+
+public class DriedfleshItem extends Item {
+	public DriedfleshItem() {
+		super(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(-1).saturationMod(-0.30000000000000004f)
+
+				.meat().build()));
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+
+		DriedfleshPlayerFinishesUsingItemProcedure.execute(entity);
+		return retval;
+	}
+}
